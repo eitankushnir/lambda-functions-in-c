a1077ec3f8c6bfd0763a2ad1b52c0dfedfff05be
eitankushnir <etn121415@gmail.com>
2026-07-17 16:17:15 +0300

    fixed lambda header

--- lambda.h
+++ lambda.h
@@ -4,10 +4,12 @@
 #if defined(__clang__) || defined(_MSC_VER)
 #define lambda_impl(ret, params, linenr, counter, func) NULL
 #elif defined(__GNUC__) || defined(__GNUG__)
-#define lambda_impl(ret, params, linenr, counter, func) \
-  ({                                                    \
-    ret concat(lmb_, linenr, counter) params func       \
-        &concat(lmb_, linenr, counter);                 \
+#define genid__lambda_helper(a, b, c) a##b##_##c
+#define genid__lambda(a, b, c) genid__lambda_helper(a, b, c)
+#define lambda_impl(ret, params, linenr, counter, func)  \
+  ({                                                     \
+    ret genid__lambda(lmb_, linenr, counter) params func \
+        &genid__lambda(lmb_, linenr, counter);           \
   })
 #endif // Compiler check for lambda_impl
 
