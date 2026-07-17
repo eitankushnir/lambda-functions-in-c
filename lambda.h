6aec9d5635a6433d23f0c9a428a41ecb2623bca8
eitankushnir <etn121415@gmail.com>
2026-07-17 16:11:35 +0300

    lambda header

---
+++ lambda.h
@@ -0,0 +1,26 @@
+#ifndef LAMBDA__H
+#define LAMBDA__H
+
+#if defined(__clang__) || defined(_MSC_VER)
+#define lambda_impl(ret, params, linenr, counter, func) NULL
+#elif defined(__GNUC__) || defined(__GNUG__)
+#define lambda_impl(ret, params, linenr, counter, func) \
+  ({                                                    \
+    ret concat(lmb_, linenr, counter) params func       \
+        &concat(lmb_, linenr, counter);                 \
+  })
+#endif // Compiler check for lambda_impl
+
+#define lambda(ret, params, arrow, func) \
+  lambda_impl(ret, params, __LINE__, __COUNTER__, func)
+
+#define lambda_expr(ret, params, arrow, expr) \
+  lambda_impl(ret, params, __LINE__, __COUNTER__, { return (expr); })
+
+#define action(func) \
+  lambda_impl(void, (void), __LINE__, __COUNTER__, func)
+
+#define fn(ret, params, func) \
+  lambda_impl(ret, params, __LINE__, __COUNTER__, func)
+
+#endif // LAMBDA__H
